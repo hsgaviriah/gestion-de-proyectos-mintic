486a0f5839b8dbdebdfd3b2acc0c3ca69d28a43b
Harold Stiven Gaviria Henao <hsgaviriah@unal.edu.co>
2021-11-02 21:22:16 -0500

    agregar boton para regresar

--- pages/admin/usuarios/index.tsx
+++ pages/admin/usuarios/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = () => {
     return (
         <div>
-            Pagina de admin de usuarios
+            <p>Pagina de admin de usuarios</p>
+            <Link href='/'>
+                <a>Ir al home</a>
+            </Link>
         </div>
     )
 }
